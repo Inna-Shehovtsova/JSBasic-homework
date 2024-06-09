30e3c9033c39bf24d41a257c156528a63247bd00
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-06-09 20:56:55 +0300

    add homework 5

--- src/hw_functions.js
+++ src/hw_functions.js
@@ -1,3 +1,4 @@
+//------------------------------------------------
 // Homework 1
 
 export function sum(a, b) {
@@ -9,10 +10,16 @@ export function mul(a, b) {
 export function strLengthSum(str1, str2) {
   return str1.length + str2.length;
 }
+//------------------------------------------------
 // Homework 2
 export function maximum(a, b) {
   return a > b ? a : b;
 }
+/** 2.Запросить у пользователя ввод числа от 1 до 12.
+ * Вывести в консоль название месяца, соответствующее
+ * этому числу (1 — январь, 2 — февраль и т.д.).
+ * @param {index} индекс месяца  от 1 до 12.
+ */
 export function mothFromIndex(index) {
   let month = "";
   switch (index) {
@@ -57,6 +64,13 @@ export function mothFromIndex(index) {
   }
   return month;
 }
+
+/** В переменных circle и square хранятся площади круга
+ * и квадрата соответственно. Написать программу,
+ * которая определяет, поместится ли круг в квадрат.
+ * @param {square} площадь квадрата
+ * @param {circle} площадь круга
+ */
 export function squareInSircle(square, circle) {
   const squareSide = Math.sqrt(square);
   const circleDiameter = circle / Math.PI;
@@ -65,4 +79,81 @@ export function squareInSircle(square, circle) {
     return true;
   }
   return false;
+}
+//------------------------------------------------
+// Homework 3
+/** сумма всех целых чисел от 50 до 100
+ */
+export function sumFromAToB(a, b) {
+  let sum1 = 0;
+  for (let i = a; i <= b; i += 1) {
+    sum1 += i;
+  }
+  return sum1;
+}
+/** таблица умножения на 7
+ * */
+export function mul7Show() {
+  let ret = "";
+  for (let i = 1; i < 10; i += 1) {
+    ret += `7 x ${i} = ${i * 7} \n`;
+  }
+  return ret;
+}
+/** среднее арифметическое всех нечётных чисел от 1 до N.
+ * @param {N} До какого числа складывать
+ */
+export function avgSumOdd(N) {
+  let sum2 = 0;
+  let count = 0;
+
+  for (let i = 1; i <= N; i += 2) {
+    sum2 = i + sum2;
+    count += 1;
+  }
+  return sum2 / count;
+}
+//------------------------------------------------
+// Homework 5
+
+/**
+ * сумму всех элементов массива
+ * @param {arr} массив
+ * @returns {sum}
+ */
+export function arrElSum(arr) {
+  return arr.reduce((s, it) => s + it, 0);
+}
+/** новый массив на основе исходного, в
+ * котором каждый элемент будет вдвое больше
+ * (a[1] = 3, b[1] = 6, где a — исходный массив, b — новый
+ * массив
+ *  @param {arr} массив
+ *  @returns {arr2}
+ */
+export function doubleArr(arr) {
+  let arr2 = [].concat(arr);
+  arr2 = arr2.map((el) => {
+    const newVal = el * 2;
+    return newVal;
+  });
+  return arr2;
+}
+/**
+ * Найдите наибольший и наименьший элементы массива
+ * @param arr массив
+ * @returns   {'min':min, 'max':max}
+ */
+export function arrMinMax(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  arr.forEach((el) => {
+    if (el < min) {
+      min = el;
+    }
+    if (el > max) {
+      max = el;
+    }
+  });
+  return { min, max };
 }
--- src/hw_functions.test.js
+++ src/hw_functions.test.js
@@ -5,6 +5,12 @@ import {
   maximum,
   mothFromIndex,
   squareInSircle,
+  sumFromAToB,
+  mul7Show,
+  avgSumOdd,
+  arrElSum,
+  doubleArr,
+  arrMinMax,
 } from "./hw_functions";
 
 describe("Sum", () => {
@@ -106,5 +112,80 @@ describe("Can square to be in circle", () => {
   });
   it("return if sircle 5  can be in square 4", () => {
     expect(squareInSircle(4, 5)).toBeTruthy();
+  });
+});
+
+describe("сумма всех целых чисел от A до B", () => {
+  it("return sum from 50 to 100 ", () => {
+    expect(sumFromAToB(50, 100)).toEqual(3825);
+  });
+  it("return sum from 0 to 0 ", () => {
+    expect(sumFromAToB(0, 0)).toEqual(0);
+  });
+  it("return sum from 0 to 1 ", () => {
+    expect(sumFromAToB(0, 1)).toEqual(1);
+  });
+  it("return sum from 1 to 3", () => {
+    expect(sumFromAToB(1, 3)).toEqual(6);
+  });
+});
+
+describe("среднее арифметическое всех нечётных чисел от 1 до N.", () => {
+  it("return sum from 0 to 1 ", () => {
+    expect(avgSumOdd(1)).toEqual(1);
+  });
+  it("return sum from 0 to 2", () => {
+    expect(avgSumOdd(2)).toEqual(1);
+  });
+  it("return sum from 0 to 3 ", () => {
+    expect(avgSumOdd(3)).toEqual(2);
+  });
+  it("return sum from 0 to 10 ", () => {
+    expect(avgSumOdd(10)).toEqual(5);
+  });
+});
+
+describe("таблица умножения на 7", () => {
+  it("таблица умножения на 7", () => {
+    expect(mul7Show()).toMatch(/7 x 1 = 7/);
+  });
+});
+
+describe("сумму всех элементов массива", () => {
+  it("сумму всех элементов массива []", () => {
+    const s = [];
+    expect(arrElSum(s)).toEqual(0);
+  });
+  it("сумму всех элементов массива [1]", () => {
+    expect(arrElSum([1])).toEqual(1);
+  });
+  it("сумму всех элементов массива [1, 3]", () => {
+    expect(arrElSum([1, 3])).toEqual(4);
+  });
+});
+
+describe("каждый элемент будет вдвое больше", () => {
+  it(" массив []", () => {
+    const s = [];
+    expect(doubleArr(s)).toStrictEqual([]);
+  });
+  it(" массив [1]", () => {
+    expect(doubleArr([1])).toStrictEqual([2]);
+  });
+  it(" массив [1, 3]", () => {
+    expect(doubleArr([1, 3])).toStrictEqual([2, 6]);
+  });
+});
+
+describe("Найдите наибольший и наименьший элементы массива", () => {
+  it(" массив []", () => {
+    const s = [];
+    expect(arrMinMax(s)).toStrictEqual({ min: undefined, max: undefined });
+  });
+  it(" массив [1]", () => {
+    expect(arrMinMax([1])).toStrictEqual({ min: 1, max: 1 });
+  });
+  it(" массив [1, 3]", () => {
+    expect(arrMinMax([1, 3])).toStrictEqual({ min: 1, max: 3 });
   });
 });
