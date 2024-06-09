11de1c1653be620d748ed2e6fe8adf2585d96643
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-06-09 12:41:29 +0300

    add sum, mul and strLengthSum functions and their tests

---
+++ src/hw_functions.js
@@ -0,0 +1,5 @@
+export function sum(a,b){return a+b;}
+export function mul(a,b){return a*b;}
+export function strLengthSum(str1,str2 ){
+    return str1.length + str2.length;
+}
---
+++ src/hw_functions.test.js
@@ -0,0 +1,50 @@
+import {sum, mul, strLengthSum} from "./hw_functions";
+
+describe ("Sum", ()=>{
+    it("return sum of two numbers", ()=>{
+        expect(sum(1,2)).toEqual(3);
+    });
+    it("return sum of two numbers", ()=>{
+        expect(sum(-1,0)).toEqual(-1);
+    });
+    it("return sum of two numbers", ()=>{
+        expect(sum(0,0)).toEqual(0);
+    });
+    it("return sum of two numbers", ()=>{
+        expect(sum(-2,2)).toEqual(0);
+    });
+});
+
+describe ("Mul", ()=>{
+    it("return multiple of two numbers", ()=>{
+        expect(mul(1,2)).toEqual(2);
+    });
+    it("return multiple of two numbers", ()=>{
+        expect(mul(-1,0)).toEqual(-0);
+    });
+    it("return multiple of two numbers", ()=>{
+        expect(mul(1,0)).toEqual(0);
+    });
+    it("return multiple of two numbers", ()=>{
+        expect(mul(0,0)).toEqual(0);
+    });
+    it("return multiple of two numbers", ()=>{
+        expect(mul(-2,2)).toEqual(-4);
+    });
+});
+
+describe ("Sum two strings length", ()=>{
+    it("return sum of str len", ()=>{
+    expect(strLengthSum('','')).toEqual(0);
+    });
+    it("return sum of str len", ()=>{
+        expect(strLengthSum('a','')).toEqual(1);
+    });
+    it("return sum of str len", ()=>{
+        expect(strLengthSum('a','b')).toEqual(2);
+    });
+    it("return sum of str len", ()=>{
+        expect(strLengthSum('test','test case')).toEqual(13);
+    });
+    
+});
