f493ae128c586760fe5b9de8df8526d0b4c328bb
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-06-09 15:35:43 +0300

    fight with jslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    sourseType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'linebreak-style': 'off',
+    'max-len': ['error', { ignoreComments: true }],
+  },
+};
