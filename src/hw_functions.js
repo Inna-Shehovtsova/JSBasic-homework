939c85ff5a69515fad91671bf0cbf0291f18f041
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-06-09 18:45:17 +0300

    add homework 2

--- src/hw_functions.js
+++ src/hw_functions.js
@@ -1,3 +1,5 @@
+// Homework 1
+
 export function sum(a, b) {
   return a + b;
 }
@@ -6,4 +8,61 @@ export function mul(a, b) {
 }
 export function strLengthSum(str1, str2) {
   return str1.length + str2.length;
+}
+// Homework 2
+export function maximum(a, b) {
+  return a > b ? a : b;
+}
+export function mothFromIndex(index) {
+  let month = "";
+  switch (index) {
+    case 1:
+      month = "January";
+      break;
+    case 2:
+      month = "February";
+      break;
+    case 3:
+      month = "Match";
+      break;
+    case 4:
+      month = "April";
+      break;
+    case 5:
+      month = "May";
+      break;
+    case 6:
+      month = "June";
+      break;
+    case 7:
+      month = "July";
+      break;
+    case 8:
+      month = "August";
+      break;
+    case 9:
+      month = "September";
+      break;
+    case 10:
+      month = "October";
+      break;
+    case 11:
+      month = "November";
+      break;
+    case 12:
+      month = "December";
+      break;
+    default:
+      throw new Error("Month index must be in 1-12!");
+  }
+  return month;
+}
+export function squareInSircle(square, circle) {
+  const squareSide = Math.sqrt(square);
+  const circleDiameter = circle / Math.PI;
+
+  if (circleDiameter < squareSide) {
+    return true;
+  }
+  return false;
 }
--- src/hw_functions.test.js
+++ src/hw_functions.test.js
@@ -1,4 +1,11 @@
-import { sum, mul, strLengthSum } from "./hw_functions";
+import {
+  sum,
+  mul,
+  strLengthSum,
+  maximum,
+  mothFromIndex,
+  squareInSircle,
+} from "./hw_functions";
 
 describe("Sum", () => {
   it("return sum of two numbers", () => {
@@ -45,5 +52,59 @@ describe("Sum two strings length", () => {
   });
   it("return sum of str len", () => {
     expect(strLengthSum("test", "test case")).toEqual(13);
+  });
+});
+
+describe("maximum", () => {
+  it("return max of two numbers", () => {
+    expect(maximum(1, 2)).toEqual(2);
+  });
+  it("return max of two numbers", () => {
+    expect(maximum(-1, 0)).toEqual(0);
+  });
+  it("return max of two numbers", () => {
+    expect(maximum(1, 0)).toEqual(1);
+  });
+  it("return max of two numbers", () => {
+    expect(maximum(110, 110)).toEqual(110);
+  });
+  it("return max of two numbers", () => {
+    expect(maximum(2, -2)).toEqual(2);
+  });
+});
+
+describe("Name of Month", () => {
+  it("return month name from index 1", () => {
+    expect(mothFromIndex(1)).toEqual("January");
+  });
+  it("return month name from index 12", () => {
+    expect(mothFromIndex(12)).toEqual("December");
+  });
+  it("return month name from index 0", () => {
+    expect(() => {
+      mothFromIndex(0);
+    }).toThrow(Error);
+  });
+  it("return month name from index 110", () => {
+    expect(() => {
+      mothFromIndex(110);
+    }).toThrow("Month index must be in 1-12!");
+  });
+  it("return month name from index 13", () => {
+    expect(() => {
+      mothFromIndex(13);
+    }).toThrow(new Error("Month index must be in 1-12!"));
+  });
+});
+
+describe("Can square to be in circle", () => {
+  it("return if sircle 1 can be in square 10", () => {
+    expect(squareInSircle(10, 1)).toBeTruthy();
+  });
+  it("return if sircle 10 can be in square 1", () => {
+    expect(squareInSircle(1, 10)).toBeFalsy();
+  });
+  it("return if sircle 5  can be in square 4", () => {
+    expect(squareInSircle(4, 5)).toBeTruthy();
   });
 });
