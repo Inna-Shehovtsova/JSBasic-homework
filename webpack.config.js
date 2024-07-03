ba107e1ed7aed621f350d53137ec1120cdffe957
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-07-03 22:31:16 +0300

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,15 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src/img"),
+    },
+    compress: true,
+    port: 9000,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "weather.js",
+    filename: "index.js",
   },
 };
