5114368ac7f04f59df2c694423a2d2625438322b
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-07-08 20:53:06 +0300

    add copy webplagin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -17,6 +18,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src/img"), to: "src/img" },
+        { from: path.resolve(__dirname, "src/style"), to: "src/style" },
+      ],
     }),
   ],
 };
