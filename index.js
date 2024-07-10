3b386c95631a89af58cbad38874018c82edcad4f
Inna-Shehovtsova <Inna-Shehovtsova@users.noreply.github.com>
2024-07-10 13:04:19 +0000

    deploy: 8af13abbaee0a8f25947c70444a5ee33e74a6ac0

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import { getWeather, drawWeather } from "./functions1.js";
+import { readList, saveList, drawList } from "./storeCity.js";
 
 (async function () {
   // Получаем указатели на нужные элементы
   const formEl = document.querySelector("form");
-  const weatherInfoEl = document.querySelector(".weather-info");
+  const weatherInfoEl = document.querySelector(".test");
+  const listEl = document.querySelector(".history");
+  let items = [];
+  // Читаем список при старте
+  // const
+  items = await readList();
+  // и отрисовываем список
+  drawList(listEl, items);
 
   function showWeather(el, weatherInfo) {
     el.innerHTML = JSON.stringify(weatherInfo, null, 2);
   }
-  // drawWeather(document.querySelector(".weater-info"), data);
 
   formEl.addEventListener("submit", async (ev) => {
     // чтобы не перезагружать страницу
@@ -18,10 +25,16 @@ import { getWeather, drawWeather } from "./functions1.js";
     const formElement = ev.target;
     const inputEl = formElement.querySelector("input");
     const cityName = inputEl.value;
+    if (cityName.trim() === "") return;
     inputEl.value = "";
-
+    items.unshift(cityName);
+    saveList(items);
     const weather = await getWeather(cityName);
+
     showWeather(weatherInfoEl, weather);
     drawWeather(document.querySelector(".weather-info"), weather);
+    // обновляем список
+    items = await readList();
+    drawList(listEl, items);
   });
 })();
